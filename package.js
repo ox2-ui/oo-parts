d1872bf127a489f55c94842f1e153ae666a8ab78
Lauricio Su <me@lauricio.com>
2015-10-04 19:35:57 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:parts',
-  summary: 'Variuos component style parts',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Variuos component style parts',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,12 +10,13 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.15'
+    'lauricio:less-autoprefixer@2.5.0_3'
     ]);
   api.addFiles('lib/oo-color-variables.import.less', C);
+  api.addFiles('lib/oo-font-variables.import.less', C);
   api.addFiles('lib/oo-header.less', C);
   api.addFiles('lib/oo-item.less', C);
   api.addFiles('lib/oo-position.less', C);
