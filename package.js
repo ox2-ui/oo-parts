c7afd9884d50b20cedd0ed8003272f1053457602
Lauricio Su <me@lauricio.com>
2015-10-05 12:39:57 +0700

    version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:parts',
   summary: 'TESTING_DO_NOT_USE Variuos component style parts',
-  version: '1.1.0',
+  version: '1.2.0',
   git: ' /* Fill me in! */ '
 });
 
